e485ef39eadfc29d24d8ec818e7895a730b4bea7
Erick Gachoka <gachokaeric819@gmail.com>
2022-01-22 06:48:25 +0300

    adds runtime comments to sort array

--- src/src/dsa/timecomplexity/SortArrayOfStrings.java
+++ src/src/dsa/timecomplexity/SortArrayOfStrings.java
@@ -14,15 +14,21 @@ public class SortArrayOfStrings {
     }
 
     void sortArray(String[] array) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = sortString(array[i]);
-        }
-        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) { // a
+            array[i] = sortString(array[i]); // O(s log s)
+        } // O(a*s log s)
+
+        Arrays.sort(array); // You need
+        // to compare the strings. Each string comparison takes O(s) time.
+        // There are O(a log a) comparisons,
+        // therefore this will take O(a*s log a) time.
+
+        // total runtime = O(a*s log s) + O(a*s log a) = O(a*s(log a + log s))
     }
 
     String sortString(String s) {
         char[] chars = s.toCharArray();
-        Arrays.sort(chars);
+        Arrays.sort(chars); // O(s log s)
         return new String(chars);
     }
 }
